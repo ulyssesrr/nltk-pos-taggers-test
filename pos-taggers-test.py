45a2ef236dbbe8cb92efb80aade9ececd1305473
Ulysses Rangel Ribeiro <ulyssesrr@gmail.com>
2015-04-02 02:12:23 -0300

    Initial import

---
+++ pos-taggers-test.py
@@ -0,0 +1,17 @@
+import importlib
+import nltk.corpus
+import os
+
+corpus = dir(nltk.corpus)
+corpus = dir(nltk.corpus)
+
+#corpus = os.listdir( nltk.data.find("corpora") )
+
+for c in corpus:
+	try:
+		mod = __import__('nltk.corpus', fromlist=[c])
+		clazz = getattr(mod, c)
+		print("%s: %d" % (c, len(clazz.tagged_words())))
+	except Exception as e:
+		#print("Erro: %s: %s" % (c,e))
+		None
