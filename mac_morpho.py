fc075dba829338420c5767fd946469e1f0337537
Ulysses Rangel Ribeiro <ulyssesrr@gmail.com>
2015-04-07 19:50:44 -0300

    Tests on Mac Morpho

---
+++ mac_morpho.py
@@ -0,0 +1,16 @@
+class OriginalMacMorphoCorpusReader:
+
+	def __init__(self):
+		self.train_tagged_sents = self.read('corpora/mac_morpho/macmorpho-train.txt')
+		self.test_tagged_sents = self.read('corpora/mac_morpho/macmorpho-test.txt')
+		
+	
+	def read(self, fname):
+		with open(fname) as f:
+			content = f.readlines()
+		#print(content[:10])
+		#print(list(map(lambda line : list(map(lambda tagged_word : tagged_word.split('_'), line.split())), content[:10])))
+		return list(map(lambda line : list(map(lambda tagged_word : tuple(tagged_word.split('_')), line.split())), content))
+		
+	def tagged_sents(self):
+		return self.train_tagged_sents + self.test_tagged_sents
--- pos-taggers-test.py
+++ pos-taggers-test.py
@@ -3,6 +3,8 @@
 import importlib
 import os
 
+from mac_morpho import OriginalMacMorphoCorpusReader
+
 import nltk.corpus
 
 import numpy as np
@@ -13,42 +15,47 @@ corpus = dir(nltk.corpus)
 corpus = dir(nltk.corpus)
 
 #corpus = os.listdir( nltk.data.find("corpora") )
-corpus = ['mac_morpho']
+#corpus = ['mac_morpho']
+corpus = [OriginalMacMorphoCorpusReader()]
 
 for c in corpus:
-	try:
-		print("Loading corpora: %s" % (c))
-		mod = __import__('nltk.corpus', fromlist=[c])
-		clazz = getattr(mod, c)
-		twords = np.array(clazz.tagged_words())
-		tsents = np.array(clazz.tagged_sents())
-		len_tsents = len(tsents)
-		print("%s: Words: %d | Sentences: %d" % (c, len(twords), len_tsents))
-		kf = KFold(len_tsents, n_folds=10)
+	#try:
+	print("Loading corpora: %s" % (c))
+	#mod = __import__('nltk.corpus', fromlist=[c])
+	#clazz = getattr(mod, c)
+	
+	tsents = np.array(c.tagged_sents())
+	twords = []
+	#print(tsents)
+	#twords = np.array(clazz.tagged_words())
+	#tsents = np.array(clazz.tagged_sents())
+	len_tsents = len(tsents)
+	print("%s: Words: %d | Sentences: %d" % (c, len(twords), len_tsents))
+	kf = KFold(len_tsents, n_folds=5)
+	
+	#tag_fd = nltk.FreqDist(tag for (word, tag) in twords)
+	tagger0 = nltk.DefaultTagger('ZZZ')
+	for k, (train_index, test_index) in enumerate(kf):
+		print("+ WK: %d" % (k))
+		tsents_train = tsents[train_index].tolist()
+		tsents_test = tsents[test_index].tolist()
 		
-		tag_fd = nltk.FreqDist(tag for (word, tag) in twords)
-		tagger0 = nltk.DefaultTagger('N')
-		for k, (train_index, test_index) in enumerate(kf):
-			print("+ K: %d" % (k))
-			tsents_train = tsents[train_index]
-			tsents_test = tsents[test_index]
+		acc = tagger0.evaluate(tsents_test)
+		print("|-Majority: %.04f" % (acc))
+		
+		tagger1 = nltk.UnigramTagger(tsents_train, backoff=tagger0)
+		acc = tagger1.evaluate(tsents_test)
+		print("|-Majority+Unigram: %.04f" % (acc))
+		
+		tagger2 = nltk.BigramTagger(tsents_train, backoff=tagger1)
+		acc = tagger2.evaluate(tsents_test)
+		print("|-Majority+Unigram+Bigram: %.04f" % (acc))
+		
+		tagger3 = nltk.TrigramTagger(tsents_train, backoff=tagger2)
+		acc = tagger3.evaluate(tsents_test)
+		print("|-Majority+Unigram+Bigram+Trigram: %.04f" % (acc))
+		exit()
 			
-			acc = nltk.tag.accuracy(tagger0, tsents_test)
-			print("|-Majority: %.04f" % (acc))
-			
-			tagger1 = nltk.UnigramTagger(tsents_train, backoff=tagger0)
-			acc = nltk.tag.accuracy(tagger1, tsents_test)
-			print("|-Majority+Unigram: %.04f" % (acc))
-			
-			tagger2 = nltk.BigramTagger(tsents_train, backoff=tagger1)
-			acc = nltk.tag.accuracy(tagger2, tsents_test)
-			print("|-Majority+Unigram+Bigram: %.04f" % (acc))
-			
-			tagger3 = nltk.TrigramTagger(tsents_train, backoff=tagger2)
-			acc = nltk.tag.accuracy(tagger3, tsents_test)
-			print("|-Majority+Unigram+Bigram+Trigram: %.04f" % (acc))
-			exit()
-			
-	except Exception as e:
-		print("Erro: %s: %s" % (c,e))
-		None
+#	except Exception as e:
+#		print("Erro: %s: %s" % (c,e))
+#		None
